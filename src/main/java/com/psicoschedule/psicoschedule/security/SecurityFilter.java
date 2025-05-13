b2425dd56f1155486bb90769f79635350e48e52c
CLARA DOS SANTOS BECKER <02178611052@edu.udesc.br>
2025-05-13 13:35:21 -0300

    organized fils

--- src/main/java/com/psicoschedule/psicoschedule/security/SecurityFilter.java
+++ src/main/java/com/psicoschedule/psicoschedule/security/SecurityFilter.java
@@ -1,36 +1,35 @@
-// package com.psicoschedule.psicoschedule.security;
+package com.psicoschedule.psicoschedule.security;
 
-// import java.io.IOException;
+import java.io.IOException;
 
-// import org.springframework.stereotype.Component;
-// import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-// import jakarta.servlet.FilterChain;
-// import jakarta.servlet.ServletException;
-// import jakarta.servlet.http.HttpServletRequest;
-// import jakarta.servlet.http.HttpServletResponse;
-// import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
-// @Component
-// public class SecurityFilter extends OncePerRequestFilter {
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
 
-//     // @Override
-//     // protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-//     //     throws ServletException, IOException {
+@Override
+protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    throws ServletException, IOException {
 
-//     //     HttpSession session = request.getSession(false); 
-//     //     String uri = request.getRequestURI();
-
-//     //     if (uri.equals("/paciente/cadastro") || uri.equals("/paciente/auth") || 
-//     //     uri.equals("/profissional/cadastro") || uri.equals("/profissional/auth")) {
-//     //         filterChain.doFilter(request, response);
-//     //         return;
-//     //     }
+    HttpSession session = request.getSession(false); 
+    String uri = request.getRequestURI();
+    if (uri.equals("/paciente/cadastro") || uri.equals("/paciente/auth") || 
+    uri.equals("/profissional/cadastro") || uri.equals("/profissional/auth")) {
+        filterChain.doFilter(request, response);
+        return;
+    }
         
-//     //     if (session != null && session.getAttribute("login") != null) {
-//     //         filterChain.doFilter(request, response);
-//     //     } else {
-//     //         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-//     //     }
-//     // }
-// }
+    if (session != null && session.getAttribute("login") != null) {
+        filterChain.doFilter(request, response);
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+    }
+}
